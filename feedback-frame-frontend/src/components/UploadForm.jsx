b348d13ab3a2e17fdc35facb78fdbf7d2d167473
epoisel <epoisel133@gmail.com>
2024-02-12 13:56:26 -0500

    fixing imports

--- feedback-frame-frontend/src/components/UploadForm.jsx
+++ feedback-frame-frontend/src/components/UploadForm.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { ref, uploadBytes, getDownloadURL } from "../firebaseConfig/storage";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { storage, firestore } from '../firebaseConfig'; // Adjust the import path as necessary
-import { collection, addDoc } from "../firebaseConfig/firestore";
+import { collection, addDoc } from "firebase/firestore";
 import { Button, Input, Select, SelectItem  } from '@nextui-org/react';
 
 function UploadForm() {
