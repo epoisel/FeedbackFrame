7915d256720476549067192ebd1f96a61f4df2b6
epoisel <epoisel133@gmail.com>
2024-02-12 13:07:48 -0500

    added environemnt variables for security

--- feedback-frame-frontend/src/firebaseConfig.js
+++ feedback-frame-frontend/src/firebaseConfig.js
@@ -5,16 +5,17 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAnpK6w0HQEe2hwuj4DmGZVgW-XEY5Dgb4",
-  authDomain: "feedback-frame.firebaseapp.com",
-  databaseURL: "https://feedback-frame-default-rtdb.firebaseio.com",
-  projectId: "feedback-frame",
-  storageBucket: "feedback-frame.appspot.com",
-  messagingSenderId: "671959290777",
-  appId: "1:671959290777:web:66f86335b457e466113908",
-  measurementId: "G-Q2TBW61KXB"
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
