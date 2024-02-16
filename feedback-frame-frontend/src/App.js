10c8b60c1fa682bebbc12414ae1f5d931999fbbe
epoisel <epoisel133@gmail.com>
2024-02-16 12:37:25 -0500

    corrected imports

--- feedback-frame-frontend/src/App.js
+++ feedback-frame-frontend/src/App.js
@@ -7,8 +7,8 @@ import Uploads from './components/Upload';
 import SignIn from './components/SignIn'; // Adjust the path as necessary
 import SignOut from './components/SignOut'; // Adjust the path as necessary
 import PasswordReset from './components/PasswordReset'; // Adjust the path as necessary
-
 import SignUp from './components/SignUp'; // Adjust the path as necessary
+import CollaborationComponent from './components/CollaborationComponent';
 import {NextUIProvider} from "@nextui-org/react";
 import './App.css';
 
