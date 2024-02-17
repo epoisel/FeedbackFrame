bcbd675066059c077f347ec39dca61f3e07e1f5e
epoisel <epoisel133@gmail.com>
2024-02-16 20:06:46 -0500

    ...

--- feedback-frame-frontend/src/App.js
+++ feedback-frame-frontend/src/App.js
@@ -9,7 +9,7 @@ import SignOut from './components/SignOut'; // Adjust the path as necessary
 import PasswordReset from './components/PasswordReset'; // Adjust the path as necessary
 import SignUp from './components/SignUp'; // Adjust the path as necessary
 import CollaborationComponent from './components/CollaborationComponent';
-import { Invitations } from './components/Invitations'; // Adjust the import path as necessary
+import  Invitations  from './components/Invitations'; // Adjust the import path as necessary
 import {NextUIProvider} from "@nextui-org/react";
 import './App.css';
 
